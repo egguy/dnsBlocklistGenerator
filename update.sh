65ff61be92d6a298c20d478055184fda0623191c
egguy <crazypops@gmail.com>
2018-12-14 14:37:09 +0100

    Add the top1M domains in the update

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+curl http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip --compressed |zcat > top-1m.csv
+
 mkdir blacklist
 rsync -arpogvtPz rsync://ftp.ut-capitole.fr/blacklist blacklist/
